51440ab6874650ef0a5a91c2aea58787d2a9aa5c
ravenmojo <105012119+ravenmojo@users.noreply.github.com>
2026-08-05 21:27:01 +0530

    feat: add Footer component to display credits and repository link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <div className="mt-8 pt-8 pb-2 border-t border-gray-200 dark:border-gray-800 text-center space-y-1.5 flex flex-col items-center justify-center font-serif">
       <p className="text-sm text-red-600 dark:text-red-500">
-        Source at <a href="https://github.com/ravenmojo/BRH_Mess_management_system" className="text-gray-900 dark:text-white hover:underline">GitHub</a>
+        OpenSourced codebase at <a href="https://github.com/ravenmojo/BRH_Mess_management_system" className="text-gray-900 dark:text-white hover:underline">GitHub</a>
       </p>
       <p className="text-sm text-red-600 dark:text-red-500 flex items-center space-x-1 justify-center">
         <span>Made with</span>
